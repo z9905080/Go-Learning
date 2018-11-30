b7a57287735e484801307183ac41230bb93aacab
shouting_hsiao <shouting_hsiao@mail.chungyo.net>
2018-11-30 13:46:33 +0800

    [新增]

---
+++ tempconv/tempconv.go
@@ -0,0 +1,12 @@
+package tempconv
+
+type Celsius float64
+type Fahrenheit float64
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
